47e9dcd8beaa609115972019f7dc54267faf24f2
acsirina <irina.a@irinas-MacBook-Pro.local>
2019-06-05 15:07:52 -0400

    push before starting deplyment to heroku

--- artt_app_api/db/seeds.rb
+++ artt_app_api/db/seeds.rb
@@ -1,81 +1,81 @@
 
 
-Past.create([
-    {
-        title: 'The Good Old Times', 
-        img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-        description: 'this was so much fun', 
-        comment:''
-    },
-    
-    { 
-        title: 'More good old times', 
-        img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-        description: 'this was so much fun', 
-        comment:''
-    },
-    
-    { 
-        title: 'Even more good old times', 
-        img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-        description: 'this was so much fun', 
-        comment:''
-    }
-])
-
-
-Present.create([
-    { 
-    title: 'The Good  Present Times', 
-    img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',description: 'this was so much fun', 
-    comment:'', 
-    date: 'January 30th 2019'
-    },
-    
-    { 
-        title: 'More good  Present Times', 
-        img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-        description: 'this was so much fun', 
-        comment:'',
-        date:'January 30th 2019'
-    },
-    
-    { 
-        title: 'Even more good  Present Times', 
-        img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-        description: 'this was so much fun', 
-        comment:'',
-        date: 'January 30th 2019'
-    }
-])
-
-# Future.create([
-#     { 
-#         title: 'The Good  future times', 
+# Past.create([
+#     {
+#         title: 'The Good Old Times', 
 #         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
 #         description: 'this was so much fun', 
-#         comment:'', 
-#         date:'January 30th 2019',
-#         attendance: false
+#         comment:''
 #     },
     
 #     { 
-#         title:'More good future Times', 
+#         title: 'More good old times', 
 #         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
 #         description: 'this was so much fun', 
-#         comment:'',
-#         date:'January 30th 2019', 
-#         attendance: false
+#         comment:''
 #     },
     
-    
 #     { 
-#         title:'Even more good  future Times', 
+#         title: 'Even more good old times', 
 #         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
 #         description: 'this was so much fun', 
-#         comment:'',
-#         date:'January 30th 2019', 
-#         attendance: false
+#         comment:''
 #     }
 # ])
+
+
+# Present.create([
+#     { 
+#     title: 'The Good  Present Times', 
+#     img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',description: 'this was so much fun', 
+#     comment:'', 
+#     date: 'January 30th 2019'
+#     },
+    
+#     { 
+#         title: 'More good  Present Times', 
+#         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
+#         description: 'this was so much fun', 
+#         comment:'',
+#         date:'January 30th 2019'
+#     },
+    
+#     { 
+#         title: 'Even more good  Present Times', 
+#         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
+#         description: 'this was so much fun', 
+#         comment:'',
+#         date: 'January 30th 2019'
+#     }
+# ])
+
+Future.create([
+    { 
+        title: 'The Good  future times', 
+        img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
+        description: 'this was so much fun', 
+        comment:'', 
+        date:'January 30th 2019',
+        attendance: false
+    },
+    
+    { 
+        title:'More good future Times', 
+        img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
+        description: 'this was so much fun', 
+        comment:'',
+        date:'January 30th 2019', 
+        attendance: false
+    },
+    
+    
+    { 
+        title:'Even more good  future Times', 
+        img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
+        description: 'this was so much fun', 
+        comment:'',
+        date:'January 30th 2019', 
+        attendance: false
+    }
+])
  
