12a20681d06c141e49bcb00fc3a8970aa58b58f9
acsirina <irina.a@irinas-MBP.home>
2019-06-21 12:37:46 -0400

    adding the right data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,81 +1,81 @@
 
 
-# Past.create([
-#     {
-#         title: 'The Good Old Times', 
-#         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-#         description: 'this was so much fun', 
-#         comment:''
-#     },
+Past.create([
+    {
+        title: 'Virginia', 
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjGV8-sZ-uuVc2kxevDwLV8uXzzr3Cjfwq_mL4-xO0vAZvWG67',
+        description: 'Great-grandmother, Virginia as a young girl back in Romania.', 
+        comment:''
+    },
     
-#     { 
-#         title: 'More good old times', 
-#         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-#         description: 'this was so much fun', 
-#         comment:''
-#     },
+    { 
+        title: 'Oldie', 
+        img: 'http://www.heritagerecipes.com/appetizer-recipes/recipes-old3.gif',
+        description: 'this was so much fun', 
+        comment:''
+    },
     
-#     { 
-#         title: 'Even more good old times', 
-#         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-#         description: 'this was so much fun', 
-#         comment:''
-#     }
-# ])
+    { 
+        title: 'Farm life', 
+        img: 'https://romaniadacia.files.wordpress.com/2015/08/old-ramnicu-sarat-romania-old-photography-romanian-peasants-farmer.jpg8',
+        description: 'Two farmers caring their hay home with faithful bulls.', 
+        comment:''
+    }
+])
 
 
-# Present.create([
-#     { 
-#     title: 'The Good  Present Times', 
-#     img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',description: 'this was so much fun', 
-#     comment:'', 
-#     date: 'January 30th 2019'
-#     },
+Present.create([
+    { 
+    title: 'Double trouble!', 
+    img: 'https://images.pexels.com/photos/158734/bird-nest-eggs-blue-158734.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&w=500',description: 'Anca and John are expecting twins! Due day to be kept secret...', 
+    comment:'', 
+    date: ''
+    },
     
-#     { 
-#         title: 'More good  Present Times', 
-#         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-#         description: 'this was so much fun', 
-#         comment:'',
-#         date:'January 30th 2019'
-#     },
+    { 
+        title: 'Is official!', 
+        img: 'https://images.pexels.com/photos/207962/pexels-photo-207962.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&w=500',
+        description: 'Maya and Rob are engaged!', 
+        comment:'',
+        date:''
+    },
     
-#     { 
-#         title: 'Even more good  Present Times', 
-#         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-#         description: 'this was so much fun', 
-#         comment:'',
-#         date: 'January 30th 2019'
-#     }
-# ])
+    { 
+        title: 'Kyle', 
+        img: 'https://images.pexels.com/photos/1146851/pexels-photo-1146851.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        description: 'We all know it, but he is officially out! Love you Kyle!', 
+        comment:'',
+        date: ''
+    }
+])
 
-# Future.create([
-#     { 
-#         title: 'The Good  future times', 
-#         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-#         description: 'this was so much fun', 
-#         comment:'', 
-#         date:'January 30th 2019',
-#         attendance: false
-#     },
+Future.create([
+    { 
+        title: 'Birthday party for Georgie', 
+        img: 'https://images.pexels.com/photos/796606/pexels-photo-796606.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&w=500',
+        description: 'Come and celebrate with us Georgies birthday party at Compo beach. Make sure to bring sunblock and a hat.', 
+        comment:'', 
+        date:'08/17/19',
+        attendance: false
+    },
     
-#     { 
-#         title:'More good future Times', 
-#         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-#         description: 'this was so much fun', 
-#         comment:'',
-#         date:'January 30th 2019', 
-#         attendance: false
-#     },
+    { 
+        title:'4th of July Party', 
+        img: 'https://images.pexels.com/photos/461917/pexels-photo-461917.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 
+        description: 'Let have a party at our house this 4th of July!',
+        comment:'',
+        date:'07/04/19', 
+        attendance: false
+    },
     
     
-#     { 
-#         title:'Even more good  future Times', 
-#         img: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/61430839_10104878858595079_8810236521312419840_n.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=8f9c4374dcb2e63d0da7e394af3dcfd3&oe=5D9BBF68',
-#         description: 'this was so much fun', 
-#         comment:'',
-#         date:'January 30th 2019', 
-#         attendance: false
-#     }
-# ])
+    { 
+        title:'Camping trip', 
+        img: 'https://images.pexels.com/photos/5921/wood-holiday-vacation-garden.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        description: 'Let start planing some outdoors adventures!', 
+        comment:'',
+        date:'07/29/19', 
+        attendance: false
+    }
+])
  
